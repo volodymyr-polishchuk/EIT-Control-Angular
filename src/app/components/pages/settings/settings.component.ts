b164d1c01f36f385fe485ce4582f999bb61dda83
polishchuk <thevivalley@gmail.com>
2019-02-22 23:21:02 +0200

    add dropdown menu

--- src/app/components/pages/settings/settings.component.ts
+++ src/app/components/pages/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,16 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  logout(): void {
+    if (confirm('Ви точно бажаєте вийти?')) {
+      localStorage.removeItem('token');
+      this.router.navigate(['login']).catch(alert);
+    }
   }
 
 }
--- src/app/components/shared/nav-bar/nav-bar.component.ts
+++ src/app/components/shared/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,4 +7,15 @@ import {Component} from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  showMenu = false;
+
+  constructor(private router: Router) { }
+
+  logout(): void {
+    if (confirm('Ви точно бажаєте вийти?')) {
+      localStorage.removeItem('token');
+      this.router.navigate(['login']).catch(alert);
+    }
+  }
+
 }
